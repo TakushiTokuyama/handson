e3555bf512d0e587b750b11b39f210283bcacf27
TakushiTokuyama <sokoninoruna@gmail.com>
2021-04-10 08:23:06 +0900

    20210410 勉強会で発表したコード

---
+++ Hash_20210410/Hash_20210410/HashCode.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Hash_20210410
+{
+    public class HashCode
+    {
+        public string HashConvert(string password)
+        {
+            // 文字コードを指定して、文字列からバイナリへの変換を行う。
+            byte[] bytes = Encoding.ASCII.GetBytes(password);
+
+            // バイナリからハッシュへと変換を行う。
+            var hash = SHA512.Create().ComputeHash(bytes);
+
+            return ByteArrayToString(hash);
+        }
+
+        static string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length; i++)
+            {
+                // ハッシュから16進数に変換する。
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+    }
+}
---
+++ Hash_20210410/Hash_20210410/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_20210410
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = new HashCode().HashConvert("1");
+
+            var password2 = new HashCode().HashConvert(password);
+
+            Console.WriteLine(password);
+
+            Console.WriteLine(password2);
+
+            if (password2 == "A0E65FEADC0BAE63AE088C3D8D648C3BBE145442E1D399618404042EE6785495DFA95844AB23CBE082A33467A96A50544DD42329CA55ADA7DF9E8CE7E0D1C740")
+            {
+                Console.WriteLine("ログイン成功");
+            }
+            else
+            {
+                Console.WriteLine("ログイン失敗");
+            }
+        }
+    }
+}
